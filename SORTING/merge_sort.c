e41bb9f62b377c21470bbcf30d1c86860ee0ec1b
hriships <hrishishinde.p@gmail.com>
2016-08-23 18:47:22 +0530

    added merge sort

--- SORTING/merge_sort.c
+++ SORTING/merge_sort.c
@@ -1,8 +1,18 @@
 #include "sorting.h"
 
-void merge_sort(int array[], int size)
-{
-	// devide the array
-	// conqure the array
-	// combine the result
+void devide_merge(int array[], int start, int end);
+void merge(int[], int start, int mid, int end);
+
+void merge_sort(int array[], int size) {
+    devide(array, 0, size);
+}
+
+void devide_merge(int array[], int start, int end) {
+    int mid;
+    if(start < end) {
+        mid = (start + end) / 2;
+        devide_merge(array, start, mid);
+        devide_merge(array, mid+1, end);
+        merge(array, start, mid, end);
+    }
 }
