41901ea27f40d7fb9d3d3ef8522ea9d769f5016f
= <=>
2016-09-02 21:02:57 +0530

    refactor: sorting driver

--- SORTING/sorting_framework.c
+++ SORTING/sorting_framework.c
@@ -11,9 +11,12 @@ void* xcalloc(int, int);
 
 /*framework functions*/
 void input(int [], int);
+void sort(int[], int);
 void test_sorting(int[], int);
+void output(int arr[], int num_of_elements);
 
-int main(int argv, char *args[]) {
+int main(int argv, char *args[]) 
+{
 
     if(argv != VALID_ARGUMENTS) {
         fprintf(stderr, "Invalid arguments: %s \n", args[1]);
@@ -23,17 +26,12 @@ int main(int argv, char *args[]) {
     int num_of_elements = atoi(args[1]);
     int* arr;
     arr = xcalloc(num_of_elements, sizeof(int));
-    srand(time(NULL));
 
+	// populates array with random numbers
     input(arr, num_of_elements);
 
-    if(num_of_elements < INSERTION_SHORT_SIZE)
-    {
-        insertion_sort(arr, num_of_elements);
-    } else {
-        merge_sort(arr, num_of_elements);
-    }
-
+	sort(arr, num_of_elements);
+	
     test_sorting(arr, num_of_elements);
 
     free(arr);
@@ -41,11 +39,13 @@ int main(int argv, char *args[]) {
     exit(EXIT_SUCCESS);
 }
 
-void* xcalloc(int instances, int size) {
+void* xcalloc(int instances, int size) 
+{
     void* m_blocks;
     m_blocks = calloc(instances, size);
    
-    if(m_blocks == NULL) {
+    if(m_blocks == NULL) 
+	{
         fprintf(stderr, "Out of memory \n");
         exit(EXIT_FAILURE);
     }
@@ -53,21 +53,38 @@ void* xcalloc(int instances, int size) {
     return m_blocks;
 }
 
-void input(int arr[], int num_of_elements) {
+void input(int arr[], int num_of_elements) 
+{
     int i;
-    for(i=0; i < num_of_elements; i++) {
+	srand(time(NULL));
+    for(i=0; i < num_of_elements; i++) 
+	{
         arr[i] = rand();    
     }
 }
 
-void test_sorting(int arr[], int size) {
+void sort(int array[], int num_of_elements)
+{
+	if(num_of_elements < INSERTION_SHORT_SIZE)
+	{   
+		insertion_sort(array, num_of_elements);
+	} 
+	else
+	{   
+		merge_sort(array, num_of_elements);
+	}
+}
+void test_sorting(int arr[], int size) 
+{
     int i;
-    for(i=1; i < size; i++) {
+    for(i=1; i < size; i++) 
+	{
         assert(arr[i-1] < arr[i]);
     }
 }
 
-void output(int arr[], int num_of_elements) {
+void output(int arr[], int num_of_elements) 
+{
     int i;
     for(i=0; i < num_of_elements; i++) {
         printf("arr[%d] = %d \n", i, arr[i]);
