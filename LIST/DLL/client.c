5cbcd1d3c19c403cbaf10d8b6ef23d2d8ba4584f
hriships <hrishishinde.p@gmail.com>
2016-11-04 10:45:15 +0530

    Refactor: insert test routines

--- LIST/DLL/client.c
+++ LIST/DLL/client.c
@@ -31,30 +31,62 @@ list_t* test_create()
 	return list;
 }
 
-
-void test_insert(list_t* list)
+void _test_insert_at_begin(list_t *list)
 {
 	result_t insert_result;
 	
 	insert_result = insert_at_begin(list, 10);
 	assert(insert_result == SUCCESS); 
 	assert(is_at_begining(list, 10) == TRUE);
+
 	insert_result = insert_at_begin(NULL, 10);
 	assert(insert_result == ERROR);
 	print_list(list);
+}
 
+void _test_insert_at_end(list_t *list)
+{
+	result_t insert_result;
+	
 	insert_result = insert_at_end(list, 40);
 	assert(insert_result == SUCCESS);
 	assert(is_at_end(list, 40) == TRUE);
+
+	insert_result = insert_at_end(NULL, 40);
+	assert(insert_result == ERROR);
 	print_list(list);
+}
+
+void _test_insert_before_data(list_t *list)
+{
+	result_t insert_result;
 
 	insert_result = insert_before_data(list, 40, 30);
 	assert(insert_result == SUCCESS);
 	assert(is_before(list, 30, 40) == TRUE);
-	print_list(list);
 
+	insert_result = insert_before_data(NULL, 40, 30);
+	assert(insert_result == ERROR);
+	print_list(list);
+}
+
+void _test_insert_after_data(list_t *list)
+{
+	result_t insert_result;
+	
 	insert_result = insert_after_data(list, 10, 20);
 	assert(insert_result == SUCCESS);
-	print_list(list);
 	assert(is_after(list, 20, 10) == TRUE);
+
+	insert_result = insert_after_data(NULL, 10, 20);
+	assert(insert_result == ERROR);
+	print_list(list);
+}
+
+void test_insert(list_t* list)
+{
+	_test_insert_at_begin(list);
+	_test_insert_at_end(list);
+	_test_insert_before_data(list);
+	_test_insert_after_data(list);
 }
